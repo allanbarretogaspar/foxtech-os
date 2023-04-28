f5ee0c98e114c8e8ffab7e0c634b7546ad40764f
Allan Barreto Gaspar <allan.gaspar@amcom.com.br>
2023-04-28 18:44:18 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/foxtech/os/FoxtechOsApplication.java
@@ -0,0 +1,13 @@
+package br.com.foxtech.os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxtechOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxtechOsApplication.class, args);
+	}
+
+}
