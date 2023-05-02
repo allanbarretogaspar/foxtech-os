3ed789b66665f73e515a613ad2af92afc14a17df
Allan Barreto Gaspar <allan.gaspar@amcom.com.br>
2023-05-02 08:43:19 -0300

    REST Testado

---
+++ src/main/java/br/com/foxtech/os/resources/ClienteResource.java
@@ -0,0 +1,17 @@
+package br.com.foxtech.os.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		
+		return "REST está Funfando!";
+	}
+
+}
